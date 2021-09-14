c785685fd88bcef27eebbd4a2eead33d9d4c755b
Natex-corporation <57963499+Natex-corporation@users.noreply.github.com>
2021-09-14 10:33:07 +0200

    repare
    repeare last columns of files

--- dataquery.py
+++ dataquery.py
@@ -17,12 +17,31 @@ for entry in obj :
         ListOfTickers.append(entry.name.split('_')[0])
 
 print(ListOfTickers[0:]) 
+<<<<<<< Updated upstream
+=======
+print(ListOfFiles[0:])
+#Ticker loading and stoaring
+
+for x in ListOfTickers:
+#get data on this ticker
+    tickerData = yf.Ticker(x)
+>>>>>>> Stashed changes
 
 class YahooAPI(object):
     def __init__(self) -> None:
         super().__init__()  
 
+<<<<<<< Updated upstream
 f = open ('LearningFiles/A_Data.csv', 'w')
+=======
+    #print (tickerDf)
+    with open('{}.csv'.format(x), 'w')as f_output:
+        tickerDf = tickerDf.iloc[: , :-2]
+        tickerDf.to_csv(f_output)
+       
+#Gathering important date for each ticker
+#Data writing in to coresponding files
+>>>>>>> Stashed changes
 
 writer = csv.writer(f)
 
