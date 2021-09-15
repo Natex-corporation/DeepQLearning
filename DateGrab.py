0af59594b233101f4f86b4c0a679c1c4b5326d9e
NAZLER_TOM <57963499+Natex-corporation@users.noreply.github.com>
2021-09-15 21:05:14 +0200

    file management changes
    .bat file for startup scan of new files

---
+++ DateGrab.py
@@ -0,0 +1,84 @@
+from datetime import date
+import pandas as pd
+import csv
+import glob
+import pandas as pd
+import pathlib
+import os
+import time
+import datetime
+import yfinance as yf
+
+
+##################################################################
+
+df = pd.read_csv("test\A.csv")
+today = date.today()
+bottom = df.tail(1)
+
+
+selact_date = df.iloc[-1, 0]
+split_date = selact_date.split('-')
+
+print (split_date)
+print (selact_date)
+
+todays_year = today.strftime("%Y")
+todays_month = today.strftime("%m")
+todays_day = today.strftime("%d")
+
+print (today)
+print (todays_year)
+print (todays_month)
+print (todays_day)
+
+
+
+if split_date[0] < todays_year :
+    print ("o rok jinde")
+    n = 1
+else:
+    if split_date[1] < todays_month :
+        print ("o měsíc jinde")
+        n = 1
+    else:
+        if split_date[2] < todays_day :
+            print ("o den jinde")
+            n = 1
+        else :
+            print ("same day ")
+
+if n == 1 :
+    path = 'LearningData'
+    ListOfTickers = []
+    ListOfFiles = []
+
+    obj = os.scandir(path)
+
+    print("files in direcotry '% s':" % path)
+    for entry in obj :
+        if entry.is_dir() or entry.is_file():
+            ListOfTickers.append(entry.name.split('_')[0])
+            ListOfFiles.append(entry.name)
+
+
+    print(ListOfTickers[0:]) 
+    print(ListOfFiles[0:])
+#Ticker loading and stoaring
+
+
+    for x in ListOfTickers:
+
+
+#get data on this ticker
+        tickerData = yf.Ticker(x)
+
+#get the historical prices for this ticker
+        tickerDf = tickerData.history(period='1d', start=selact_date, end=today)
+
+    #print (tickerDf)
+        with open('test/{}.csv'.format(x), 'w')as f_output:
+            tickerDf = tickerDf.iloc[: , :-2]
+            tickerDf.to_csv(f_output, line_terminator='\n')
+
+today
---
+++ pointer.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Python39\python.exe" "D:\AA tomovy projekty\programi\ProjectsInProgress\DeepQlearning\DateGrab.py"
+pause
